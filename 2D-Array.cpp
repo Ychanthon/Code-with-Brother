74eac5cd79b3cdb536985eb839d32683d628492d
Y.CHANTHON <y.chanthon.it@gmail.com>
2025-02-11 01:18:52 +0700

    Update 2D-Array.cpp

--- 2D-Array.cpp
+++ 2D-Array.cpp
@@ -1,44 +1,33 @@
 #include <iostream>
 #include <string>
-
 using namespace std;
-
-int main() {
-    int n; // Number of products
-
+int main()
+{
+    int n;
     cout << "Enter the number of products: ";
     cin >> n;
-
-    // Create a 2D array to store product information
-    string products[n][4]; // 4 fields: ID, Name, Import Date, Exp Date
-
-    // Input product details
-    for (int i = 0; i < n; i++) {
+    string products[n][4];
+    for (int i = 0; i < n; i++)
+    {
         cout << "\nEnter details for product " << i + 1 << ":" << endl;
-
         cout << "Product ID: ";
         cin >> products[i][0];
-
         cout << "Product Name: ";
-        cin.ignore(); // Clear the input buffer
+        cin.ignore();
         getline(cin, products[i][1]);
-
         cout << "Product Import Date (YYYY-MM-DD): ";
         cin >> products[i][2];
-
         cout << "Product Expiry Date (YYYY-MM-DD): ";
         cin >> products[i][3];
     }
-
-
     cout << "\nProduct Information:" << endl;
     cout << "---------------------------------------------" << endl;
     cout << "ID\tName\t\tImport Date\tExpiry Date" << endl;
     cout << "---------------------------------------------" << endl;
 
-    for (int i = 0; i < n; i++) {
+    for (int i = 0; i < n; i++)
+    {
         cout << products[i][0] << "\t" << products[i][1] << "\t\t" << products[i][2] << "\t" << products[i][3] << endl;
     }
-
     return 0;
 }
